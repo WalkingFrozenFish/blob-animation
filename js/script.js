ce1bc37c8deda9be14334d38ae8cd653177a5ff2
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-03-17 14:39:30 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const tween = KUTE.fromTo(
+    "#blob1",
+    { path: "#blob1" },
+    { path: "#blob2" },
+    {
+        repeat: 999,
+        duration: 3000,
+        yoyo: true
+    }
+)
+
+tween.start();
